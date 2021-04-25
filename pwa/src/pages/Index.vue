6c9fab20306d43618f1285886e8193ce9438efce
cameron <cameron.p@cla.aero>
2021-04-25 06:12:12 +0000

    initial quasar pwa version

---
+++ pwa/src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      stats: []
+    }
+
+  },
+  mounted () {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      try {
+        const result = await this.$axios.get({}, '/api', {
+          auth: {
+            username: 'selpi',
+            password: 'selpi'
+          }
+        })
+        this.stats = result.data
+      }
+      finally {
+      }
+    },
+  }
+}
+</script>
